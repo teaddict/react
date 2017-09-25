b86382ded00aef60f10abd1de01908efd1f20338
teaddict <cyberasker@gmail.com>
2017-09-25 21:39:35 +0300

    learning jsx

--- hello-react/public/app.jsx
+++ hello-react/public/app.jsx
@@ -1,4 +1,35 @@
+var Greeter = React.createClass({
+	render: function() {
+		return(
+			<div>
+				<h1>Hello React!</h1>
+			</div>
+		);
+	}
+});
+
+var Body = React.createClass({
+	render: function() {
+		return(
+			<div>
+				<p>This is new paragraph! It is body component.</p>
+			</div>
+		);
+	}
+});
+
+var ReactExample = React.createClass({
+	render: function() {
+		return React.createElement(
+			'h1',
+			null,
+			'This element created with React createElement function'
+		)
+	}
+});
+
+
 ReactDOM.render(
-	<h1>Hello React!</h1>,
+	<div><Greeter/><ReactExample/><Body/></div>,
 	document.getElementById('app')
 );
